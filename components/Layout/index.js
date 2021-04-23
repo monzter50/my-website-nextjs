b9c5ea1d5e39b7bd7abee25c7836dacea8269357
Jose Gonzalez <josemonzter50@gmail.com>
2021-04-23 18:18:18 -0500

    implement react

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import styles from "./Layout.module.css";
 import Navbar from "../Navbar"
 import Footer from "../Footer"
--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import styles from "./Navbar.module.css";
 
 export default function Navbar () {
