6384e418f940802be9f4703879ba354a22b961bc
jose antonio <joseantonio.gonzalez@globant.com>
2022-03-26 21:46:36 -0600

    migrate typescript

--- .history/next.config_20220324155302.js
+++ .history/next.config_20220324155302.js
@@ -8,12 +8,5 @@ module.exports = {
       }
   
       return config
-    },
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-      ignoreBuildErrors: true,
-    },
+    }
   }
