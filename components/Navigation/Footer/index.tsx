03728b24029169407a212b67daa64320500307e8
monster <josemonzter50@gmail.com>
2022-09-10 10:21:14 -0500

    feature: add structure footer

--- components/Navigation/Footer/index.tsx
+++ components/Navigation/Footer/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import styles from "./Footer.module.css";
 
-export default function Footer () {
+export default function Footer() {
   return (
-      <div className={styles.footer}>
-        <div className="book-info">
-         <p>Footer</p>
+    <div className={styles.footer}>
+      <div className={styles.container}>
+        <div className={styles.contact}>
+          <img className={styles.isotipo} src="https://s3.amazonaws.com/monstercodes.dev/assets/isotipo.svg" alt="Monster Codes" />
+          <a className={styles.email} href="mailto:jglez.dev@gmail.com">jglez.dev@gmail.com</a>
         </div>
+        <div className={styles.copyright}>© 2021-present Jose Gonzalez Castro. All Rights Reserved.</div>
       </div>
+
+    </div>
   )
 }
