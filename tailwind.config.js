1ea4d7df2878b03a59024df443531e65c34c7af2
Jose A. Gonzalez <jglez.dev@gmail.com>
2023-08-13 12:39:56 -0500

    chore: add variable and config for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,27 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        roboto:['var(--font-family)']
+      },
+      colors: {
+        primary:"var(--first-color)",
+        secondary:"var(--second-color)",
+        neutral:"var(--neutral-color)",
+        "blue-dark":"var(--blue-dark)",
+        blue:"var(--blue)",
+        "blue-contrast":"var(--blue-contrast)",
+        "blue-ligth":"var(--black-contrast)",
+        "light-grey":"var(--light-grey)",
+        grey:"var(--grey)"
+      },
+      fontWeight: {
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+      }
+    },
   },
   plugins: [],
 }
